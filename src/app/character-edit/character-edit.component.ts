dc790ecfbf2763fdc06531217f09e695ddd5eeda
PATRIK\lokos <lokospatrik8@gmail.com>
2025-05-13 15:52:47 +0200

    create character edit component

---
+++ src/app/character-edit/character-edit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-character-edit',
+  imports: [],
+  templateUrl: './character-edit.component.html',
+  styleUrl: './character-edit.component.scss'
+})
+export class CharacterEditComponent {
+
+}
